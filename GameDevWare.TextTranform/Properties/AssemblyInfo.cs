49a544a6931cf76a035629ba3256e6f6dfbf906a
shai_hulud <deniszykov@gmail.com>
2016-12-27 22:35:19 +0300

    changed package name

--- GameDevWare.TextTranform/Properties/AssemblyInfo.cs
+++ GameDevWare.TextTranform/Properties/AssemblyInfo.cs
@@ -21,8 +21,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("GameDevWare.TextTransform")]
-[assembly: AssemblyDescription("T4 text tranformation tools")]
+[assembly: AssemblyTitle("T4 CLI")]
+[assembly: AssemblyDescription("Command line utility for text transformation with using T4 templates. ")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GameDevWare")]
 [assembly: AssemblyProduct("GameDevWare.TextTransform")]
