a8f47f62b162a606dfa8020022fbe6212db57684
Denis Zykov <deniszykov@gmail.com>
2019-04-20 19:23:00 +0300

    moved all code to project directory

---
+++ src/GameDevWare.TextTransform/Json/JsonType.cs
@@ -0,0 +1,12 @@
+// ReSharper disable once CheckNamespace
+namespace Assets.Editor.GameDevWare.TextTransform.Json
+{
+	internal  enum JsonType
+	{
+		String,
+		Number,
+		Object,
+		Array,
+		Boolean
+	}
+}
